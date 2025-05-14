491915fe8b4d1e2e87043568bcfd57f20067ffa2
Matias Camiletti <matias.camiletti@gmail.com>
2025-05-14 16:21:47 -0300

    Add TotsUnimatrixCode model extending TotsUserCode

---
+++ src/Models/TotsUnimatrixCode.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tots\Unimatrix\Models;
+
+use Tots\AuthTfaBasic\Models\TotsUserCode;
+
+/**
+ * Description of Model
+ *
+ * @author matiascamiletti
+ */
+class TotsUnimatrixCode extends TotsUserCode
+{
+    const PROVIDER_UNIMATRIX = 1;
+}
